07e51a881f1f0b290c894860262b70de75d79628
sjml2002 <sjml2002@naver.com>
2021-06-29 00:50:25 +0900

    처음 로딩시 서버 연결 안되면 알림 표시

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Loading from "./View/Loading";
 import * as GetLocation from "./Model/getLocation"; //getData함수 호출
+import MapViewHome from './View/MapView.js';
+
+
+export default function ScreenNavigation(url) {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="HomeScreen"
+        headerMode="screen"
+      >
+        <Stack.Screen name="HomeScreen" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="MapView" component={MapViewHome}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 
 //React 컴포넌트 class를 정의하려면 React.Component를 상속받아야함
-export default class extends React.Component {
+class HomeScreen extends React.Component {
   state = {
     isLoading: true,
+    LotsofTimedPassed: false, //true면은 너무 많은시간이 로딩된것임
     location: {
       latitude: null,
       longitude: null,
@@ -17,25 +38,51 @@ export default class extends React.Component {
 
   getLocation = async() => {
     const locationData = await GetLocation.getLocation();
-    const result = await GetLocation.locationDBInsert(locationData); //locationData DBinsert
-    console.log("result", result);
+    await GetLocation.locationDBInsert(locationData); //locationData DBinsert
     this.setState({
       isLoading: false,
       location: locationData,
     });
   }
 
+  NavigatorSetting(screenName, sendData) {
+    if(sendData !== null) {
+      this.props.navigation.navigate(
+        screenName,
+        {data: sendData}
+      );
+    }
+    else{
+      this.props.navigation.navigate(screenName);
+    }
+  }
+
   componentDidMount() {
     this.getLocation();
+    setTimeout(
+      function() { 
+        this.setState({LotsofTimedPassed: true});
+      }.bind(this),
+      4000
+    );
   }
 
   render() {
     const isLoading  = this.state.isLoading;
     const locationData = this.state.location;
+    const lotp = this.state.LotsofTimedPassed;
     if(isLoading) {
-      return (
-        <Loading />
-      );
+      if(lotp) {
+        return(
+          Alert.alert("서버와 연결할 수 없습니다."),
+          <Loading />
+        );
+      }
+      else{
+        return (
+          <Loading />
+        );
+      }
     }
     else{
       if(locationData == null) {
@@ -50,8 +97,15 @@ export default class extends React.Component {
       else{
         return (
           <View style={styles.container}>
-            <Text style={styles.text}> 왜 안뜨지? </Text>
             <Text style={styles.text}> {locationData.latitude} / {locationData.longitude} </Text>
+            <View style={styles.mapContainer}>
+              <TouchableOpacity
+                onPress={() => this.NavigatorSetting("MapView", locationData)}
+                style={styles.mapButton}
+              >
+                <Text style={[styles.text, {fontSize: 15}]}>go to map</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         );
       }
@@ -62,6 +116,9 @@ export default class extends React.Component {
   }*/
 }
 
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -71,5 +128,16 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     color: 'black',
+    textAlign: "center",
+  },
+  mapContainer: {
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  mapButton: {
+    justifyContent: "center",
+    width: 100,
+    height: 30,
+    backgroundColor: "blue",
   }
 });
