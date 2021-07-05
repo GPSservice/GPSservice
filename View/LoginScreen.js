a27fa85e43734f599c706c6a397f75b93413069f
sjml2002 <sjml2002@naver.com>
2021-07-05 14:58:58 +0900

    login.js -> LoginScreen.js 파일명 변경

--- View/LoginScreen.js
+++ View/LoginScreen.js
@@ -17,6 +17,7 @@ async function chekcLogin(userID, userPW, {navigation}) {
     }
 }
 
+//loginScreen (export default function)
 function NewReminderScreen ({navigation}) {
       const [username, setUsername] = useState("");
       const [password, setPassword] = useState("");
