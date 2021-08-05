bd79b1e7fbf0bdf343d613dcb3c0cd9d757ccf81
sjml2002 <sjml2002@naver.com>
2021-08-05 22:11:23 +0900

    age, job에 Picker추가 및 서버 request구현

--- View/Register.js
+++ View/Register.js
@@ -2,20 +2,72 @@ import 'react-native-gesture-handler';
 import React, {useState} from "react";
 import { AntDesign } from '@expo/vector-icons'
 import SimpleSelectButton from 'react-native-simple-select-button';
+import {Picker} from '@react-native-picker/picker';
 import {StyleSheet, Text, View, TouchableOpacity, TextInput, Button, Alert, Image, FlatList} from "react-native";
+
+import * as RegisterRequest from "../Model/postRegister";
+
+async function registerServerRequest(data, {navigation}) {
+  const getMessage = await RegisterRequest.SignupDBInsert(data);
+  if(getMessage.result) {
+    Alert.alert("회원가입이 완료되었습니다."); 
+    navigation.navigate("Login");
+  }
+  else{
+    Alert.alert(getMessage.errorMessage);
+  }
+}
+
+function checkRegister(data, {navigation}) {
+  //회원가입 양식 확인
+  const idReg = /^[a-zA-Z]+[a-zA-Z0-9]{5,15}$/;
+  const pwReg = /^[a-zA-Z]+[a-zA-Z0-9~!@$%*]{7,19}$/;
+  //데이터 확인
+  if(data.id=="" || data.pw=="" || data.cfpw=="" || data.gender=="" || data.job=="" || data.age=="") {
+    //데이터 중 누락된것이 있으면
+    Alert.alert("전부 입력을 해주세요.");
+  } 
+  else if(!idReg.test(data.id)) {
+    //id 정규식 확인
+    Alert.alert("ID는 영문자로 시작하는 5~15자 영문자 또는 숫자이어야 합니다.");
+  }
+  else if(!pwReg.test(data.pw)){
+    Alert.alert(
+      "비밀번호는 영문자로 시작하는 8~20자 영문자,숫자 또는 특수문자이어야 합니다.",
+      "특수문자 허용: ~, !, @, $, %, *"
+    );
+  }
+  else if(data.pw!==data.cfpw) {
+    Alert.alert("비밀번호와 비밀번호 확인이 동일하지 않습니다.");
+  }
+  else {
+    //입력된것에 이상이 없으면 서버통신 진행
+    registerServerRequest(data, {navigation});
+    //회원가입 성공 여부는 이 함수에서 확인진행
+  }
+}
 
 function RegisterScreen ({navigation}) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [cf_password, setcfPassword] = useState("");
     const [sex, setSex] = useState("");
-    const [job, setJob] = useState("");
-    const [age, setAge] = useState("");
+    const [job, setJob] = useState("Baeksu");
+    const [age, setAge] = useState("10");
     const button_list = [
       { label: "Male", value: "male" },
       { label: "Female", value: "female" },
       { label: "Unknown", value: "unknown" },
     ];
+    const sendData = {
+      id: username,
+      pw: password,
+      cfpw: cf_password,
+      gender: sex,
+      job: job,
+      age: age,
+    };
+
       return (
               <View style={styles.container}>
               <Text style = {{fontWeight : 'bold', fontSize: 25, color: '#E6E6FA', marginBottom: 5}}>Register</Text>
@@ -54,7 +106,6 @@ function RegisterScreen ({navigation}) {
                           
                       }
                     />
-
               </View>
 
                 <Text style = {{fontWeight : 'bold', fontSize: 15, color: '#E6E6FA', marginBottom: 5 }}>Password</Text>
@@ -73,27 +124,52 @@ function RegisterScreen ({navigation}) {
                     secureTextEntry={true}
                     style={styles.textbox}
                   />
-                <Text style = {{fontWeight : 'bold', fontSize: 15, color: '#E6E6FA', marginBottom: 5 }}>Job</Text>
-                  <TextInput
-                    value={this.state.job}
-                    onChangeText={(value) => setJob(value)}
-                    placeholder={'Job'}
-                    style={styles.textbox}
-                  />
+                  {/* job */}
+                <View>
+                  <Text style = {{fontWeight : 'bold', fontSize: 15, color: '#E6E6FA', marginBottom: 5 }}>Job</Text>
+                <View>
+                  <Picker
+                    selectedValue={job}
+                    onValueChange={(itemValue, itemIndex) =>
+                      setJob(itemValue)
+                    }
+                  >
+                    <Picker.Item label="무직" value="Baeksu"/>
+                    <Picker.Item label="중,고등학생" value="SchoolStudent"/>
+                    <Picker.Item label="대학생" value="UniversityStudent"/>
+                    <Picker.Item label="취준생" value="JobSeeker"/>
+                    <Picker.Item label="직장인" value="Worker"/>
+                    <Picker.Item label="프리랜서" value="Freelancer"/>
+                    <Picker.Item label="자영업자, 임원" value="CEO"/>
+                    <Picker.Item label="공무원" value="OfficialWorker"/>
+                    <Picker.Item label="그 외" value="etc"/>
+                  </Picker>
+                </View>
+                </View>
+                  {/* age */}
                 <Text style = {{fontWeight : 'bold', fontSize: 15, color: '#E6E6FA', marginBottom: 5 }}>Age</Text>
-                  <TextInput
-                    value={this.state.age}
-                    onChangeText={(value) => setAge(value)}
-                    placeholder={'Age'}
-                    style={styles.textbox}
-                  />
+                  <Picker
+                      selectedValue={age}
+                      onValueChange={(itemValue, itemIndex) =>
+                        setAge(itemValue)
+                      }
+                    >
+                    <Picker.Item label="10대" value="10"/>
+                    <Picker.Item label="20대" value="20"/>
+                    <Picker.Item label="30대" value="30"/>
+                    <Picker.Item label="40대" value="40"/>
+                    <Picker.Item label="50대" value="50"/>
+                    <Picker.Item label="60대" value="60"/>
+                    <Picker.Item label="70대 이상" value="70"/>
+                  </Picker>
+
+
                     <View style={styles.btnContainer}>
                       {/* DB에 넘기기 (if password === cf_password)*/}
                       {/* DB에 잘 넘어갔는지 check */}
                     <View style ={styles.goto}>
-                      <TouchableOpacity onPress={() => {
-                        Alert.alert("회원가입이 완료되었습니다."); 
-                        navigation.navigate("Login");}} 
+                      <TouchableOpacity 
+                        onPress={() => checkRegister(sendData, {navigation})} 
                         style={styles.buttons}>
                         <Text style={{fontSize: 20, color: "#000000"}}>confirm</Text>
                       </TouchableOpacity>
